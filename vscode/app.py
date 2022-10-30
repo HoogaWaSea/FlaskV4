8b1dc8f4e5380014aa0996ed6fe0295385f936b4
Hooga <hoosheng0614@gmail.com>
2022-10-29 23:16:34 -0700

    first

---
+++ vscode/app.py
@@ -0,0 +1,58 @@
+from cgi import test
+from urllib import request
+from flask import Flask, request
+
+app = Flask(__name__)
+
+stores = [
+    {
+        "name": "My Store",
+        "items": [
+            {
+                "name": "Chair",
+                "price": 15.99
+            }
+        ]
+    }
+]
+
+@app.get("/store")
+def get_stores():
+    return {"stores": stores}
+
+
+@app.post("/store")
+def create_store():
+    request_data = request.get_json()
+    new_sotre = {"name": request_data["name"], "items": []}
+    stores.append(new_sotre)
+    return new_sotre, 201
+
+@app.post("/store/<string:name>/item")
+def create_item(name):
+    request_data = request.get_json()
+    for store in stores:
+        if store["name"] == name:
+            new_item = {"name": request_data["name"], "price": request_data["pice"]}
+            store["items"].append(new_item)
+            return new_item, 201
+
+    return {"message": "Store not found"}, 404
+
+
+
+@app.get("/store/<string:name>")
+def get_store(name):
+    for store in stores:
+        if store["name"] == name:
+            return store
+    return {"message": "Store not found"}, 404
+
+@app.get("/store/<string:name>/item")
+def get_item_in_store(name):
+    for store in stores:
+        if store["name"] == name:
+            return {"item": store["items"], "message": "You rock!"}
+    return {"message": "Store not found"}, 404
+
+# test2
